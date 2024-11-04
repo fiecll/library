e8bf7b92edea72e35406158f4607fffd554c4766
fie <doublerokko@yahoo.co.jp>
2024-11-04 22:37:22 +0900

    a

--- components/Layout.js
+++ components/Layout.js
@@ -6,8 +6,6 @@ const Layout = ({ children }) => {
             <header>
                 <nav>
                     <Link href="/">Home</Link> | 
-                    <Link href="/algorithms/sorting">Sorting Algorithm</Link> | 
-                    <Link href="/algorithms/dijkstra">Dijkstra Algorithm</Link>
                 </nav>
             </header>
             <main>{children}</main>
