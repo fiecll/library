35622dbedc198a2967792745bea22d3a4a5c5b36
fie <doublerokko@yahoo.co.jp>
2024-11-04 23:01:48 +0900

    a

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
+import Link from 'next/link';
+import algorithmLinks from '../components/algorithmLinks'; // 自動生成されたリンクリスト
+
 const Home = () => {
-  return (
-      <div>
-          <h1>Kyopro Library</h1>
-          <p>競技プログラミング用のアルゴリズムとデータ構造のライブラリです。</p>
-      </div>
-  );
+    return (
+        <div className="container">
+            <h1>Kyopro Library</h1>
+            <p>競技プログラミングで使えるアルゴリズムとデータ構造のライブラリです。</p>
+            <ul className="algorithm-list">
+                {algorithmLinks.map((link) => (
+                    <li key={link.name}>
+                        <Link href={link.url}>{link.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Home;
